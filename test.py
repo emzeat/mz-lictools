7a30f780671c44d19c2be92df66bb196dcf034e2
Marius Zwicker <marius.zwicker@mlba-team.de>
2023-04-19 07:14:58 +0200

    Fix parsing of files without whitespace after header
    This may happen after a manual edit or when the header was originally
    added manually or by another tool
    
    Fixes #7 on Github.

--- test.py
+++ test.py
@@ -251,6 +251,17 @@ class TestParserPoundStyle(unittest.TestCase):
         self.assertEqual(
             "cmake_minimum_required(VERSION 3.0.0 FATAL_ERROR)", parsed.remainder)
 
+    @parser_test(BASE / 'test/TestParserPoundStyle-no_space.py')
+    def test_no_space(self, parsed):
+        self.assertEqual(1, len(parsed.authors))
+        self.assertEqual("Max Muster", parsed.authors[0].name)
+        self.assertEqual(2010, parsed.authors[0].year_from)
+        self.assertEqual(2010, parsed.authors[0].year_to)
+        self.assertEqual(license_tools.Style.DOCSTRING_STYLE, parsed.style)
+        self.assertTrue(parsed.license.startswith(
+            "This library is"), parsed.license)
+        self.assertEqual("import unittest", parsed.remainder)
+
     @parser_test(BASE / 'test/TestParserTaggedPoundStyle-1author_1year.cmake')
     def test_tagged_1author_1year(self, parsed):
         self.assertEqual(1, len(parsed.authors))
